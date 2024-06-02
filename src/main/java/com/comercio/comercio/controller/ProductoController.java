1bab71f079cdcf20791a6909a7de10021c286287
Mateo Gutierrez Moreno <mateoalejandro02a@gmail.com>
2024-06-02 15:20:10 -0500

    subida de archivos testeada junto con su imagen

--- src/main/java/com/comercio/comercio/controller/ProductoController.java
+++ src/main/java/com/comercio/comercio/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.comercio.comercio.controller;
 import com.comercio.comercio.model.Producto;
 import com.comercio.comercio.model.Usuario;
 import com.comercio.comercio.service.ProductoService;
+import com.comercio.comercio.service.UploadFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 
 
 @Controller
@@ -22,6 +26,9 @@ public class ProductoController {
 
     @Autowired
     private ProductoService productoService;
+
+    @Autowired
+    private UploadFileService upload;
 
     @GetMapping("")
     public String show(Model model){
@@ -35,10 +42,16 @@ public class ProductoController {
     }
 
     @PostMapping("/save")
-    public String save(Producto producto){
+    public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("Este es el objeto producto {}", producto);
         Usuario u = new Usuario(1,"usuario","","","",null,"","");
         producto.setUsuario(u);
+
+        if (producto.getId()==null){
+            String nombreImagen = upload.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }
+
         productoService.save(producto);
         return "productos/show";
     }
